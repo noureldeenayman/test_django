06938e95f07522b00119574de36c64cad4660aa5
nour eldeen <na060553@gmail.com>
2025-05-04 19:30:44 +0300

    initinal commit

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'draft', 'Draft'
+        PUBLISHED = 'published', 'Published'
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=Status.choices, default=Status.DRAFT)
+    tags = models.CharField(max_length=100, blank=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    body = models.TextField()
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+    def __str__(self):
+        return self.title
