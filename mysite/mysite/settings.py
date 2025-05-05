dfa06f6b6af32e68a4d2620aecda71b59fc2296f
nour eldeen <na060553@gmail.com>
2025-05-05 16:24:59 +0300

    initinal commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -111,11 +111,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # مجلد التجميع لوضع الإنتاج
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # مجلد عام للملفات الثابتة
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
     path('blog/', include('blog.urls', namespace='blog')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
