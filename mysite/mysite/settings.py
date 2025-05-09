1edfa8de9fb8541bc577be9743f01c189f878a61
nour eldeen <na060553@gmail.com>
2025-05-09 10:15:32 +0300

    initinal commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -114,12 +114,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # مجلد التجميع لوضع الإنتاج
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static', 
- ] # مجلد التجميع لوضع الإنتاج
-  
+    BASE_DIR / 'blog' / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
